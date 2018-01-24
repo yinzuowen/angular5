b54f82ebb6a4f383ecb3d97e3ea546a0db25f981
yinzuowen <591684535@qq.com>
2018-01-24 15:54:05 +0800

    angular5第一次提交。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { IndexComponent } from './index/index.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { Page4Component } from './page4/page4.component';
+import { Page404Component } from './page404/page404.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        IndexComponent,
+        Page1Component,
+        Page2Component,
+        Page3Component,
+        Page4Component,
+        Page404Component
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
